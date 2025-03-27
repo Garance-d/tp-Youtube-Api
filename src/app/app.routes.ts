e7ee5a896c37f5cd9e7463031ed4ef2fae08cf74
Garance <davidgarance0@gmail.com>
2025-03-27 15:19:24 +0100

    Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PlayerComponent } from './components/player/player.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SearchComponent } from './components/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'player', component: PlayerComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'search', component: SearchComponent},
+];
--- src/app/partials/header/header.component.ts
+++ src/app/partials/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isLoggedIn: boolean = false
 
 }
