db772e2d3dbf1c98719997b081aa3ad50598057b
Garance <davidgarance0@gmail.com>
2025-03-27 13:56:24 +0100

    Création components, partial, pages,service, guards + Tailwinds. 27/03/2025

---
+++ src/app/components/user-api/user-api.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-api',
+  imports: [],
+  templateUrl: './user-api.component.html',
+  styleUrl: './user-api.component.scss'
+})
+export class UserApiComponent {
+
+}
