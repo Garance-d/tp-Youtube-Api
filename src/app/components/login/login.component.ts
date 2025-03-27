100dad85733332981f08bb2b3df551adc06c4981
Garance <davidgarance0@gmail.com>
2025-03-27 18:08:33 +0100

    Formulaire inscription + connexion

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  monFormulaire = new FormGroup({
+    nom: new FormGroup('', Validators.required),
+  password: new FormGroup('', Validators.required)
+  });
+
+  onSubmit() {
+    if (this.monFormulaire.valid) {
+      const formData ={
+        nom: this.monFormulaire.value.nom,
+        password: this.monFormulaire.value.password,
+      };
+      console.log(formData)
+      
+    } else {
+      console.log("Formulaire invalide")
+      
+    }
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  monFormulaire = new FormGroup({
+    nom: new FormGroup('', Validators.required),
+  password: new FormGroup('', Validators.required),
+  email: new FormGroup('', Validators.required)
+  });
+
+  onSubmit() {
+    if (this.monFormulaire.valid) {
+      const formData ={
+        nom: this.monFormulaire.value.nom,
+        password: this.monFormulaire.value.password,
+        email: this.monFormulaire.value.email
+      };
+      console.log(formData)
+      
+    } else {
+      console.log("Formulaire invalide")
+      
+    }
+  }
 
 }
